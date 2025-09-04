ba7d4bd70d3ea455c6a54f33da506ad4c9ad6a60
Trifonko Vyacheslav 350502 <vtrifonko@gmail.com>
2025-09-04 17:39:42 +0300

    e)fix d)

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -15,10 +15,10 @@ public class ComplexTest {
     }
 
     @Test
-    public void testPLus() {
+    public void testPlus() {
         Complex a = new Complex(5, 3);
         Complex b = new Complex(2, 1);
-        Complex result = a.minus(b);
+        Complex result = a.plus(b);
         assertEquals(3, result.getRe(), 1e+10);
         assertEquals(2, result.getIm(), 1e+10);
     }
